0837853cc2fd78314af89d70da8dae1e24abfe7c
Young-Ho Cha <ganadist@gmail.com>
2014-02-16 01:42:56 +0900

    update copyright
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- warp.py
+++ warp.py
@@ -4,7 +4,7 @@
 VERSION = "v0.1 (poc code)"
 
 """
-Copyright (c) 2013 devunt
+Copyright (c) 2014 devunt
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
